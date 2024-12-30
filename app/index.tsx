9685f61836897e0fa495cad7a00c5dd514e58e5f
VoidIfPerceived <sophi@offnominal.com>
2024-12-30 02:19:48 -0600

    reset application, installed nativewind and dependencies

---
+++ app/index.tsx
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View } from 'react-native'
+import { Link } from 'expo-router';
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+})
